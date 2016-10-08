9a082cd0e6f662f350e6ea9a7346e6db616eb64d
Jamie Harvey <jharvey@duality.co.nz>
2016-10-08 21:37:30 +1300

    Initial commit

---
+++ app/Sustainabot/app/src/main/java/nz/co/duality/sustainabot/RobotRemote.java
@@ -0,0 +1,13 @@
+package nz.co.duality.sustainabot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RobotRemote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_robot_remote);
+    }
+}
